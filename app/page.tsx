ca74199ae768b22459d1a30c40ad240b7366ec13
chiaradenardi <chiara.denardi2@studio.unibo.it>
2024-11-09 11:48:41 +0100

    base progetto

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+// app/page.tsx
+import React from 'react';
+
+const Page = () => {
+  return (
+    <div>
+      <h1>Welcome to My Next.js App with TypeScript!</h1>
+    </div>
+  );
+};
+
+export default Page;
